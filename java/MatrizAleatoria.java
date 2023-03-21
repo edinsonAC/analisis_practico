fc72f964b7a7af94279b2882ed1797656e107ae5
Edinson Antolinez <edinsonarveyac@ufps.edu.co>
2023-03-20 22:02:35 -0500

    terminando las pruebas de node

--- java/MatrizAleatoria.java
+++ java/MatrizAleatoria.java
@@ -8,7 +8,7 @@ public class MatrizAleatoria {
         System.out.println(filas + " " + cols);
         for (int i = 0; i < filas; i++) {
             for (int j = 0; j < cols; j++) {
-                matriz[i][j] = (int) (Math.random() * 100);
+                matriz[i][j] = (int) (Math.random() * 1000);
                 if (j == matriz[i].length - 1) {
                     System.out.print(matriz[i][j]);
                 } else {
